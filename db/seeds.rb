225b83ab0422926fb44b4f4b2c7894e8be1f2ab2
Al Palaran <aljade1902@gmail.com>
2023-07-22 15:18:11 +0100

    reformat index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,12 @@
 User.create(email: 'test@mail.com', first_name: 'Test', last_name: 'User',
             password: '123', password_confirmation: '123')
+
+if User.count < 20
+  User.create(
+    email: 'test@mail.com',
+    first_name: 'Test',
+    last_name: 'User',
+    password: '123',
+    password_confirmation: '123'
+  )
+end
