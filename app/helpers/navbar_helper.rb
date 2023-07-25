a7ac332c4d210e6340cde3163c2ad1c8fc7562e6
Al Palaran <aljade1902@gmail.com>
2023-07-25 23:01:30 +0100

    remove redundancy in navbar code

---
+++ app/helpers/navbar_helper.rb
@@ -0,0 +1,21 @@
+module NavbarHelper
+  def admin_nav_items
+    [
+      ['Users', admin_users_path],
+      ['Teams', admin_teams_path],
+      ['Events', admin_events_path],
+      ['Blogs', admin_blogs_path],
+      ['Dashboard', admin_dashboard_index_path]
+    ]
+  end
+
+  def user_nav_items
+    [
+      ['Contact', events_path],
+      ['Blogs', blogs_path],
+      ['Events', events_path],
+      ['Teams', teams_path],
+      ['Leaderboard', leaderboard_index_path]
+    ]
+  end
+end
