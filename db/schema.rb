b98c423eb9b283e79405ea6736061db2abac6ad7
Eugen Rupi <aconime.soft@gmail.com>
2023-07-27 12:48:08 +0100

    dashboard with sidebar + other UI fixes

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_182917) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.boolean "published", default: false
+    t.boolean "published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
